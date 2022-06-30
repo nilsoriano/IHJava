53bc8c43b3eb77c0d4964d98f7c89f576475dbf2
Sen Evel <nilsorianovila@gmail.com>
2022-06-30 12:42:02 +0000

    Getters & Setters finished.

--- classesandobjects/Main.java
+++ classesandobjects/Main.java
@@ -5,10 +5,16 @@ package classesandobjects;
 */
 class Main {
   public static void main(String[] args){
+    /*
     Book book1 = new Book("The Pragmatic Programmer", "David Thomas & Andrew Hunt", 321);
     Book book2 = new Book("Software Craftsmanship", "Sandro Mancuso", 305);
     
     System.out.println("The book " + book1.getTitle() + " contains " + book1.getLength() + " pages.");
-    System.out.println("The book " + book2.getTitle() + " contains " + book2.getLength() + " pages.");
+    System.out.println("The book " + book2.getTitle() + " contains " + book2.getLength() + " pages."); 
+    */
+    Friend friend1 = new Friend ("Alex", "Pérez" , "Granollers", true);
+    Friend friend2 = new Friend ("Sandra", "López", "Tossa de Mar", false);
+    
+    System.out.println(friend2.getFirstName() + " " + friend2.getLastName() + " was born in " + friend2.getBirthplace() + ". The fact that their profile is publicly visible is " + friend2.isPublic() + ".");
   }
 }
