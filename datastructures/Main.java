185d0b0577a834a74ec0fd7753201c56609ade35
Sen Evel <nilsorianovila@gmail.com>
2022-07-03 10:48:49 +0000

    Lists completed.

--- datastructures/Main.java
+++ datastructures/Main.java
@@ -1,7 +1,27 @@
 package datastructures;
-class Main {
+import java.util.List;
+import java.util.ArrayList;
+/* This compiles, but does not run using repl.it's button.
+   To run it, paste this into the console:
+   java -classpath .:target/dependency/* datastructures/Main
+*/
 
-  public static void main (String[[] args]) {
-    
+class Main {
+  public static void main (String[] args) {
+    List<String> brandList = new ArrayList<>();
+
+    brandList.add("Mitsurrari");
+    brandList.add("Seault");
+    brandList.add("Mazvo");
+    brandList.add("Koenigoyce");
+
+    for (int i = 0; i < brandList.size(); i++) {
+      System.out.println(brandList.get(i));
+    }
+
+    brandList.remove(2);
+    for (int i = 0; i < brandList.size(); i++) {
+      System.out.println(brandList.get(i));
+    }
   }
 }
