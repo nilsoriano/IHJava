319440f64a700024be6074977c57445f0c38bb5c
Sen Evel <nilsorianovila@gmail.com>
2022-07-03 17:58:40 +0000

    Exercise selector added.

--- datastructures/Main.java
+++ datastructures/Main.java
@@ -1,4 +1,5 @@
 package datastructures;
+import java.util.Scanner;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.ArrayList;
@@ -9,7 +10,39 @@ import java.util.HashMap;
 */
 
 class Main {
-  public static void main (String[] args) {
+  // Lists
+  static void DataStructures1() {
+    List<String> brandList = new ArrayList<>();
+    brandList.add("Mitsurrari");
+    brandList.add("Seault");
+    brandList.add("Mazvo");
+    brandList.add("Koenigoyce");
+    
+    for (int i = 0; i < brandList.size(); i++) {
+      System.out.println(brandList.get(i));
+    }
+
+    brandList.remove(2);
+    for (int i = 0; i < brandList.size(); i++) {
+      System.out.println(brandList.get(i));
+    }
+  }
+
+  // Lists II
+  static void DataStructures2() {
+    List<Contact> contactList = new ArrayList<>();
+    contactList.add(new Contact("Juan",611622633));
+    contactList.add(new Contact("Ana",622633644));
+    contactList.add(new Contact("Juan",633644655));
+    
+    for (int i = 0; i < contactList.size(); i++) {
+      System.out.println(contactList.get(i).getName());
+      System.out.println(contactList.get(i).getPhone());
+    }
+  }
+
+  // Maps
+  static void DataStructures3(){
     List<Pet> perseusPets = new ArrayList<>();
     perseusPets.add(new Pet("Pegasus V2", 2557));
     perseusPets.add(new Pet("Bellepheron's refactored Pegasus", 2764));
@@ -27,6 +60,29 @@ class Main {
       for (Pet pN : o.getValue()) {
         System.out.println(pN.getName());
       }
+    }
+  }
+  
+  // Main
+  public static void main(String[] args) {
+    Scanner exerciseSelector = new Scanner(System.in);
+    System.out.println("Select the Prework snippet you wish to run (1-3)");
+    int userSelection = exerciseSelector.nextInt();
+    exerciseSelector.close();
+    
+    switch (userSelection) {
+      case 1:
+        DataStructures1();
+        break;
+      case 2:
+        DataStructures2();
+        break;
+      case 3:
+        DataStructures3();
+        break;
+      default:
+        System.out.println("Input is not within expected values.");
+        break;
     }
   }
 }
